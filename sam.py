8cd1b68880a1e0430ad5e1f898325a3c99a8d9b6
Rahul143 <Rahul143@gmail.com>
2021-02-16 16:52:13 +0000

    new file uploded

---
+++ sam.py
@@ -0,0 +1,13 @@
+<html>
+<head> This is sample code</head>
+<h1>my Second sample to crete html</h1>
+<body>
+
+<table style="width:100%>
+  <tr>
+     <th>firstname</th>
+     <th>secondname</th>
+  </tr>
+</table>
+</body>
+</html>
